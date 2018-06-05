17c9875ddf0a97ba65cd249c8a61352448a5281a
Per Steen Olsen <persteen22@hotmail.com>
2018-06-05 14:49:40 +0200

    Adding JSX React File

---
+++ CoreTwoZeroJS/wwwroot/js/reactfile.jsx
@@ -0,0 +1,14 @@
+﻿
+class Greeting extends React.Component {
+    render() {
+        return (<p>Hello world from ReactJS, by loading a jsx React component...</p>);
+    }
+     
+    
+}
+
+
+ReactDOM.render(
+    <Greeting />,
+    document.getElementById('root')
+);
